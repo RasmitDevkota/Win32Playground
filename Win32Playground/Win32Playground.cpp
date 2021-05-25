307774c4b41a45d38e10f9aaf91bbb4263272e89
Rasmit Devkota <alientechgamig@hotmail.com>
2021-05-25 14:05:22 -0400

    CURL request to webpage

--- Win32Playground/Win32Playground.cpp
+++ Win32Playground/Win32Playground.cpp
@@ -12,8 +12,36 @@
 #define UNICODE
 #endif
 
+int historyDepth = 0;
+
+HDC hdc;
+PAINTSTRUCT ps;
+
+RECT cRect;
+
+LONG width;
+LONG height;
+
+LONG borderA;
+LONG borderB;
+LONG borderC;
+
+HWND window;
+
+RECT newTab;
+RECT tabsAreaRect;
+RECT controlsAreaRect;
+RECT extrasAreaRect;
+RECT mainContentRect;
+
 HWND urlBar;
 HWND searchButton;
+
+WCHAR* url[2048] = {};
+
+std::string pageData;
+
+HWND pageContent;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 HWND CreateRichEdit(HWND hwndOwner, int x, int y, int width, int height);
@@ -31,7 +59,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindow(
+    window = CreateWindow(
         CLASS_NAME,
         L"OneE",
         WS_OVERLAPPEDWINDOW,
@@ -39,12 +67,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL, NULL, hInstance, NULL
     );
 
-    if (hwnd == NULL)
+    if (window == NULL)
     {
         return 0;
     }
 
-    ShowWindow(hwnd, nCmdShow);
+    ShowWindow(window, nCmdShow);
     
     MSG msg = { };
     while (GetMessage(&msg, NULL, 0, 0))
@@ -70,7 +98,7 @@ HWND CreateRichEdit(HWND hwndOwner, int x, int y, int width, int height) {
     HWND hwnd = CreateWindowEx(
         0,
         MSFTEDIT_CLASS,
-        TEXT("Type a URL"),
+        L"Type a URL",
         ES_MULTILINE | WS_VISIBLE | WS_CHILD | WS_BORDER | WS_TABSTOP,
         x, y, width, height,
         hwndOwner, (HMENU) 102, NULL, NULL
@@ -99,9 +127,29 @@ HWND CreateButton(HWND hwndOwner, int x, int y, int width, int height) {
     return hwnd;
 }
 
+HWND CreateContent(HWND hwndOwner) {
+    const wchar_t CLASS_NAME[] = L"OneE Page Content";
+
+    WNDCLASS wc = { };
+
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    pageContent = CreateWindow( 
+        L"STATIC",
+        L"Enter a URL into the address bar and hit go to begin exploring the web!",
+        WS_VISIBLE | WS_CHILD | SS_LEFT,
+        width * 0.005L, borderC + height * 0.025L * (historyDepth + 1L), width * 0.995L, height * 0.995L,
+        hwndOwner, (HMENU) 519, NULL, NULL
+    );
+
+    return pageContent;
+}
+
 size_t requestData(char* buffer, size_t itemSize, size_t nItems, void* pageData) {
     size_t nBytes = itemSize * nItems;
-    
+
     OutputDebugStringA(buffer);
     OutputDebugString(L"\n");
 
@@ -120,18 +168,16 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             return 0;
         }
         case WM_CREATE: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
+            hdc = BeginPaint(hwnd, &ps);
 
-            RECT cRect;
             GetClientRect(hwnd, &cRect);
 
-            LONG width = cRect.right - cRect.left;
-            LONG height = cRect.bottom - cRect.top;
+            width = cRect.right - cRect.left;
+            height = cRect.bottom - cRect.top;
 
-            LONG borderA = height * 0.05L;
-            LONG borderB = borderA + height * 0.05L;
-            LONG borderC = borderB + height * 0.04L;
+            borderA = height * 0.05L;
+            borderB = borderA + height * 0.05L;
+            borderC = borderB + height * 0.04L;
 
             urlBar = CreateRichEdit(hwnd, 0.05L * width, borderA + 0.1L * (borderB - borderA), 0.85L * width, 0.8L * (borderB - borderA));
             searchButton = CreateButton(hwnd, 0.9L * width, borderA + 0.1L * (borderB - borderA), 0.05L * width, 0.8L * (borderB - borderA));
@@ -141,26 +187,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             return 0;
         }
         case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
+            hdc = BeginPaint(hwnd, &ps);
 
             FillRect(hdc, &ps.rcPaint, CreateSolidBrush(RGB(217, 89, 140)));
 
-            RECT cRect;
-            GetClientRect(hwnd, &cRect);
-
-            LONG width = cRect.right - cRect.left;
-            LONG height = cRect.bottom - cRect.top;
-
-            LONG borderA = height * 0.05L;
-            LONG borderB = borderA + height * 0.05L;
-            LONG borderC = borderB + height * 0.04L;
-
-            RECT tabsAreaRect;
             SetRect(&tabsAreaRect, 0, 0, width, borderA);
             FillRect(hdc, &tabsAreaRect, CreateSolidBrush(RGB(42, 42, 42)));
 
-            RECT newTab;
             SetRect(&newTab, 0.01 * width, 0, 0.08 * width, borderA);
             FillRect(hdc, &newTab, CreateSolidBrush(RGB(26, 26, 26)));
 
@@ -171,59 +204,18 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             HFONT font = CreateFont(18, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial");
             HFONT oldHFont = (HFONT) SelectObject(hdc, font);
 
-            TextOut(hdc, 0.015 * width, 0.75 * borderA, L"New Tab", 7);
+            if (historyDepth == 0) {
+                TextOut(hdc, 0.015 * width, 0.75 * borderA, L"New Tab", 7);
+            }
 
-            RECT controlsAreaRect;
             SetRect(&controlsAreaRect, 0, borderA, width, borderB);
             FillRect(hdc, &controlsAreaRect, CreateSolidBrush(RGB(28, 28, 28)));
 
-            RECT extrasAreaRect;
             SetRect(&extrasAreaRect, 0, borderB, width, borderC);
             FillRect(hdc, &extrasAreaRect, CreateSolidBrush(RGB(31, 31, 31)));
 
-            RECT mainContentRect;
             SetRect(&mainContentRect, 0, borderC, width, height);
             FillRect(hdc, &mainContentRect, CreateSolidBrush(RGB(255, 255, 255)));
-
-            std::string pageData;
-
-            CURL* curl = curl_easy_init();
-
-            curl_easy_setopt(curl, CURLOPT_URL, "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBVT22t-x2H76119AHG8SgPU0_A0U-N1uA\0");
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "email=rdatch101@gmail.com&password=jangwonyoung&returnSecureToken=true");
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, requestData);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &pageData);
-
-            CURLcode result = curl_easy_perform(curl);
-
-            if (result == CURLE_OK) {
-                OutputDebugString(L"successfully did curl stuff\n");
-
-                SetTextColor(hdc, RGB(0, 0, 0));
-                SetBkColor(hdc, RGB(255, 255, 255));
-
-                HFONT font = CreateFont(18, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial");
-                HFONT oldHFont = (HFONT) SelectObject(hdc, font);
-
-                // TextOutA(hdc, width * 0.005L, borderC + height * 0.025L, pageData.c_str(), pageData.size());
-
-                // DrawTextExA(hdc, (LPSTR) pageData.c_str(), pageData.size(), &contentRect, DT_CALCRECT, NULL);
-
-                RECT contentArea;
-                SetRect(&contentArea, width * 0.025L, width * 0.975L, borderC + height * 0.025L, height * 0.975L);
-                ExtTextOutA(hdc, width * 0.025L, borderC + height * 0.025L, ETO_CLIPPED, &contentArea, (LPSTR) pageData.c_str(), pageData.size(), NULL);
-
-                OutputDebugString(L"drew curl stuff\n");
-
-                EndPaint(hwnd, &ps);
-            }
-            else {
-                OutputDebugString(L"unsuccessfully did curl stuff\n");
-                OutputDebugStringA(curl_easy_strerror(result));
-                OutputDebugString(L"\n");
-            }
-
-            curl_easy_cleanup(curl);
 
             EndPaint(hwnd, &ps);
 
@@ -231,14 +223,46 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         }
         case WM_COMMAND: {
             if (wParam == 831) {
-                OutputDebugStringA("Button pressed");
-
-                WCHAR* url[2048];
-
                 GetWindowTextA(urlBar, (LPSTR) url, 2048);
 
-                OutputDebugStringA((LPSTR) url);
+                CURL* curl = curl_easy_init();
+
+                curl_easy_setopt(curl, CURLOPT_URL, url);
+
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, requestData);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &pageData);
+
+                ((std::string*) &pageData)->clear();
+
+                CURLcode result = curl_easy_perform(curl);
+
+                if (result == CURLE_OK) {
+                    OutputDebugString(L"successfully did curl stuff\n");
+
+                    SetTextColor(hdc, RGB(0, 0, 0));
+                    SetBkColor(hdc, RGB(255, 255, 255));
+
+                    HFONT font = CreateFont(18, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, L"Arial");
+                    HFONT oldHFont = (HFONT) SelectObject(hdc, font);
+
+                    if (historyDepth == 0) {
+                        CreateContent(hwnd);
+                    }
+
+                    SetWindowTextA(pageContent, pageData.c_str());
+
+                    historyDepth++;
+                }
+                else {
+                    OutputDebugString(L"unsuccessfully did curl stuff\n");
+                    OutputDebugStringA(curl_easy_strerror(result));
+                    OutputDebugString(L"\n");
+                }
+
+                curl_easy_cleanup(curl);
             }
+
+            return 0;
         }
     }
 
